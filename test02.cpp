5a273477312637b24134d77c1669622cae861a2a
ljw1A <3355428852@qq.com>
2024-08-03 16:35:53 +0800

    test02.cpp

--- test02.cpp
+++ test02.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+class son{
+  protected:
+    int a;
+    public:
+    son(int x){
+        a=x;
+    }
+    void show(){
+        cout<<a<<endl;
+    }
+};
+
+class father : public son{
+    int b;
+    public:
+    father(int x,int y):son(x),b(y){}
+    void show(){
+        cout<<a<<" "<<b<<endl;
+    }
+};
+
+
 int main() {
-    cout << "Hello, World!" << endl;
+    father f(1,2);
+    f.show();
+
+
+
     return 0;
 }
